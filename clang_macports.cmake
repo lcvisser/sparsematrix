59f83a8dbe839d8be2a0280d2c9afeca6a1862e6
lcvisser <ludo.visser@gmail.com>
2022-11-14 21:34:38 +0100

    Setting up for running tests and coverage report

--- clang_macports.cmake
+++ clang_macports.cmake
@@ -1,5 +1,5 @@
-set( CMAKE_SYSTEM_NAME Darwin )
+set(CMAKE_SYSTEM_NAME Darwin)
 
-set( COMPILER_BIN /opt/local/bin )
-set( CMAKE_C_COMPILER ${COMPILER_BIN}/clang CACHE PATH "clang" )
-set( CMAKE_CXX_COMPILER ${COMPILER_BIN}/clang++ CACHE PATH "clang++" )
+set(COMPILER_BIN /opt/local/bin)
+set(CMAKE_C_COMPILER ${COMPILER_BIN}/clang CACHE PATH "clang")
+set(CMAKE_CXX_COMPILER ${COMPILER_BIN}/clang++ CACHE PATH "clang++")
