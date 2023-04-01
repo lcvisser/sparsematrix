82f29d7507bb2295e5ba90c00d2baa263afeb541
lcvisser <ludo.visser@gmail.com>
2023-04-01 23:31:28 +0200

    Parse coverage result

--- bin/build-gcc.sh
+++ bin/build-gcc.sh
@@ -3,20 +3,24 @@
 set -euxo pipefail
 
 SRCDIR="$(pwd)/sparsematrix"
+BUILDDIR="$(pwd)/build"
 GCOV="${GCOV:=gcov}"
+MIN_COVERAGE=90
 
 echo "SRCDIR=$SRCDIR"
+echo "BUILDDIR=$BUILDDIR"
 echo "CC=$CC"
 echo "CXX=$CXX"
 echo "GCOV=$GCOV"
 
 # Build
-rm -rf build
+rm -rf $BUILDDIR
 cmake --preset default-configure-coverage
 cmake --build --preset default-build-coverage
+echo "*\n!.gitignore\n" > $BUILDDIR/.gitignore
 
 # Prepare for test
-cd build
+cd $BUILDDIR
 lcov --zerocounters --directory .
 
 # Run tests
@@ -25,7 +29,13 @@ ctest
 # Process coverage data
 lcov --capture --directory testbin/ --include "$SRCDIR/sparsematrix.h" --gcov-tool $GCOV --output-file coverage.info
 lcov --list coverage.info
-
+coverage=$(lcov --list coverage.info | grep -e '^sparsematrix.h' | awk -F'[[:space:]]+' '{gsub(/[^[:digit:]\.]/, "", $2); print $2}')
+if (( $(echo "$coverage > $MIN_COVERAGE" | bc -l) ))
+then
+    echo "Coverage OK"
+else
+    echo "Coverage NOT OK ($coverage)"
+fi
 
 # Generate HTML report
 genhtml --output-directory coverage_html coverage.info
